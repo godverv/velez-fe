3e441c7adcb4dc41062baf878b09cab2453f4b6c
AlexSkilled <olegthander@yandex.ru>
2024-12-12 16:17:29 +0300

    [master] icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import '@/index.module.css'
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+
+import {ThemeProvider} from '@gravity-ui/uikit';
+
+import App from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme="light">
     <App />
-  </StrictMode>,
+  </ThemeProvider>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// @ts-expect-error
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    base: '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  });
+}
