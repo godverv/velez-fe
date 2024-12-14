fe79d0e990635ab8cb9f220f43c17175ada50a73
AlexSkilled <olegthander@yandex.ru>
2024-12-14 23:40:32 +0300

    [master] closable tabs

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,11 +4,24 @@ import {useState} from "react";
 import ControlPageWidget from "@/widgets/services/ControlPlane";
 
 export default function HomePage() {
-   const [openedWidget, setOpenedWidget] = useState<React.JSX.Element>(<ControlPageWidget/>)
+    const [openedWidget, setOpenedWidget] = useState<React.JSX.Element>(<ControlPageWidget/>)
+
     return (
         <div className={styles.Home}>
-            <div>HEADER</div>
-            {openedWidget}
+            <div className={styles.HomeHeader}>
+                <div className={styles.Tab}>
+                    Home
+                </div>
+                <div className={styles.Tab}>
+                    <div className={styles.TabName}>Matreshka</div>
+                    <div className={styles.CloseButton}>
+                    <span className="material-symbols-outlined" children={"close"}/>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.OpenedWidget}>
+                {openedWidget}
+            </div>
         </div>
     )
 }
